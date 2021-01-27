574516c623d9574c98c297275dbe9cd59df29307
bgdelcampo <73716016+bgdelcampo@users.noreply.github.com>
2021-01-27 16:48:24 +0100

    fixed bug

--- src/scrip.js
+++ src/scrip.js
@@ -17,19 +17,21 @@ let currentMin = now.getMinutes();
 let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = `${dayWeek} ${currentHour}:${currentMin}`;
 
-// display forecast
-function displayForecast (response){
-  console.log (response.data);
-}
+
 
 // Temp per city
-
 function goCity(event) {
   event.preventDefault();
   let currentLocation = document.querySelector("#location");
   let inputCity = document.querySelector("#city-name");
+  let city = `${inputCity.value}`;
   currentLocation.innerHTML = `${inputCity.value}`;
-  searchCity(inputCity.value);
+  let apiKey = `38c9293a2365a91b362e3d61894095ed`;
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+
+  axios.get(apiUrl).then(showTemperature);
   apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -40,7 +42,6 @@ function defaultCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
 }
-
 
 function showTemperature (response) {
   let temp = Math.round(response.data.main.temp);
@@ -65,7 +66,8 @@ function showTemperature (response) {
 let searchCity = document.querySelector("#button-submit");
 searchCity.addEventListener("click", goCity);
 
-// --°C and °F--
+
+
 
 function displayFahrenheitTemperature (event) {
   event.preventDefault();
@@ -91,5 +93,6 @@ let celsiuslink =document.querySelector ("#celsius-link");
 celsiuslink.addEventListener("click", displayCelsiusTemperature);
 
 let celsiusTemperature = null;
+
 
 defaultCity("Dresden");
