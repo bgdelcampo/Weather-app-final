7d6708bb779e06154080eca96dd7036e98234e7b
bgdelcampo <73716016+bgdelcampo@users.noreply.github.com>
2021-01-27 16:36:39 +0100

    fiished project

--- src/scrip.js
+++ src/scrip.js
@@ -17,20 +17,30 @@ let currentMin = now.getMinutes();
 let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = `${dayWeek} ${currentHour}:${currentMin}`;
 
+// display forecast
+function displayForecast (response){
+  console.log (response.data);
+}
 
 // Temp per city
+
 function goCity(event) {
   event.preventDefault();
   let currentLocation = document.querySelector("#location");
   let inputCity = document.querySelector("#city-name");
-  let city = `${inputCity.value}`;
   currentLocation.innerHTML = `${inputCity.value}`;
-  let apiKey = `38c9293a2365a91b362e3d61894095ed`;
+  searchCity(inputCity.value);
+  apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function defaultCity(city) {
+    let apiKey = `38c9293a2365a91b362e3d61894095ed`;
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-
-
   axios.get(apiUrl).then(showTemperature);
+  apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 
@@ -56,30 +66,6 @@ function showTemperature (response) {
 
 let searchCity = document.querySelector("#button-submit");
 searchCity.addEventListener("click", goCity);
-
-
-// fetch description
-
-
-
-// Add a Current Location button
-
-// function fetchPosition (position){
-//   let apiKey = `38c9293a2365a91b362e3d61894095ed`;
-//   let units = "metric";
-//   let lat = position.coords.latitude;
-//   let lon = position.coords.longitude;
-//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-//   axios.get(url).then(showTemperature);
-  
-// }
-
-
-// let locationButton = document.querySelector ("#search-location");
-// locationButton.addEventListener("click", fetchPosition);
-// navigator.geolocation.getCurrentPosition(fetchPosition);
-
-
 
 // --°C and °F--
 
@@ -108,28 +94,4 @@ celsiuslink.addEventListener("click", displayCelsiusTemperature);
 
 let celsiusTemperature = null;
 
-goCity ("Dresden");
-
-
-// function convertCelsius(event) {
-//   event.preventDefault();
-//   let tempCelsius = document.querySelector("#current-weather");
-//   tempCelsius.innerHTML = "12°C";
-// }
-
-// let celsiusUnits = document.querySelector("#celsius-link");
-// celsiusUnits.addEventListener("click", convertCelsius);
-
-// function convertFarenheit(event) {
-//   event.preventDefault();
-//   let tempFarenheit = document.querySelector("#current-weather");
-
-//   tempFarenheit.innerHTML = "53.6°F";
-// }
-
-// let farenheitUnits = document.querySelector("#farenheit-link");
-// farenheitUnits.addEventListener("click", convertFarenheit);
-
-
-
-// API
+defaultCity("Dresden");
